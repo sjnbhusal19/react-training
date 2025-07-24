e77cc749e28f36b6e9d8350e8225b93760443c2a
Sujan Bhusal <sujan.bhusal@gmail.com>
2025-07-24 11:12:53 +0545

    update

---
+++ loops.js
@@ -0,0 +1,31 @@
+//for loop
+
+
+// for (initialization; condition; finalExpression) {
+//   // Code to be executed in each iteration
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+
+// let obj = {
+// "item":"apple",
+// "color": "red"
+// }
+
+//string 
+//1. .length 
+//2.   index of the string ---     str[1]
+//Template literls   ``
+
+
+// const str = `this is developers ${word.count}`
+
+//this process is called string interpolations
+
+// .toUpperCase()
+// .toLowerCase()
+// .trim()   //remove the whitespace
+// 
