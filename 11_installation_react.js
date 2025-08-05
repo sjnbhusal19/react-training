785ae6ad90b1def1d8a992174859c47624667d82
Sujan Bhusal <sujan.bhusal@gmail.com>
2025-08-05 10:17:04 +0545

    added roadmap for react

---
+++ 11_installation_react.js
@@ -0,0 +1,16 @@
+//installation of react form vite 
+
+//explain every folder structure and inside the file also
+
+//explain how to write code or locic in jsx file like variable, function , conditional statement, array, objects etc
+
+// show all the variable , function , conditional , array , list , table , objects etc in inside the return statement to show in ui
+
+//show how we can write js inside the curly bracket {}
+
+//also used .map , .filter inside the retun
+
+//then explain about the components and how we use component in the pages 
+
+//explain props 
+
