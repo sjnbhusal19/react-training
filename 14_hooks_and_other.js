9d39b86720c60b6e6b75583723485fe80586d140
Sujan Bhusal <sujan.bhusal@gmail.com>
2025-08-11 11:25:03 +0545

    update teaching

---
+++ 14_hooks_and_other.js
@@ -0,0 +1,20 @@
+//useState
+const [stateVariable, setStateVariable] = useState(initialValue);
+
+//useEffect
+
+useEffect(() => {
+  // Code to run (side effect)
+
+  return () => {
+    // Optional cleanup code
+  };
+}, [dependencies]);
+
+
+
+
+//make a simple form which incluede input, select , button , radio ,select , date etc and submit and show in console
+
+
+
